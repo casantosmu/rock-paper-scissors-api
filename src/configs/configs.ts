42fb41b9f26ffe656dbd7c6726d4c8256894f476
Carlos <casantosmu@gmail.com>
2022-10-07 03:52:34 +0200

    Added joinRoomController

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -4,6 +4,13 @@ const configs = {
     hostName: process.env.HOST_NAME,
     frontendWebUrl: process.env.FRONTEND_WEB_URL,
   },
+  eventMessages: {
+    room: {
+      joinBase: "join_game",
+      joinSucces: "join_game_success",
+      joinError: "join_game_error",
+    },
+  },
 } as const;
 
 export default configs;
