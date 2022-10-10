7c53405f1cc05647151304dc9000ef2c92b8e727
Carlos <casantosmu@gmail.com>
2022-10-11 00:51:13 +0200

    Added uploadIsStarted move service

--- src/services/moveServices.ts
+++ src/services/moveServices.ts
@@ -1,4 +1,9 @@
+import { FilterQuery, UpdateQuery } from "mongoose";
+import configs from "../configs/configs";
 import Room from "../database/models/Room";
+import { RoomWithId } from "../types/interfaces";
+
+const { gameSettings } = configs;
 
 const addUserWaiting = async (roomId: string, userId: string) => {
   await Room.findByIdAndUpdate(roomId, {
@@ -12,7 +17,26 @@ const removeUserWaiting = async (roomId: string, userId: string) => {
   });
 };
 
+const uploadIsStarted = async (roomId: string) => {
+  const lessThanOneToStart = gameSettings.playersPerRoom - 1;
+
+  const filter: FilterQuery<RoomWithId> = {
+    _id: roomId,
+    isStarted: false,
+    [`usersWaiting.${lessThanOneToStart}`]: { $exists: true },
+  };
+  const update: UpdateQuery<RoomWithId> = {
+    $set: {
+      isStarted: true,
+      usersWaiting: [],
+    },
+  };
+
+  return Room.findOneAndUpdate(filter, update);
+};
+
 export default {
   addUserWaiting,
   removeUserWaiting,
+  uploadIsStarted,
 };
